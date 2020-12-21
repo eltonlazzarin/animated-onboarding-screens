2c30bd76c6c46134473499681c6ab6e726e99157
Elton Lazzarin <elton.lazzarin@outlook.com>
2020-12-21 09:03:11 -0300

    styling main page

---
+++ src/pages/Main/styles.js
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  max-width: 1220px;
+  margin: 64px auto;
+
+  h3 {
+    font-size: 40px;
+    font-weight: bold;
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  main {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 32px;
+    padding-top: 32px;
+  }
+
+  article {
+    width: 281px;
+    height: 420px;
+  }
+
+  img {
+    height: 300px;
+    width: 281px;
+    margin-bottom: 16px;
+    object-fit: cover;
+  }
+
+  p {
+    font-size: 16px;
+    font-weight: normal;
+    padding: 4px 0 10px 0;
+  }
+
+  footer {
+    display: flex;
+    align-items: center;
+  }
+
+  svg {
+    cursor: pointer;
+    margin-right: 13px;
+  }
+`;
