4f90d6f0a7862e2502efb54db4f2ea90cb4429ce
Elton Lazzarin <elton.lazzarin@outlook.com>
2020-12-22 08:29:42 -0300

    add routes to new naver and edit naver

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Redirect, Route } from 'react-router-dom';
 
 import Logon from './pages/Logon';
 import Main from './pages/Main';
+import NewNaver from './pages/NewNaver';
+import EditNaver from './pages/EditNaver';
 
 function Routes() {
   return (
@@ -10,6 +12,8 @@ function Routes() {
       <Redirect exact from="/" to="/logon" />
       <Route path="/logon" component={Logon} />
       <Route path="/navers" component={Main} />
+      <Route path="/createnaver" component={NewNaver} />
+      <Route path="/editnaver/:id" component={EditNaver} />
     </BrowserRouter>
   );
 }
