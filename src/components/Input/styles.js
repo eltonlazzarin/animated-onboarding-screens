4ed1717adae5750bf5d1fbdc17485affe7cd10b2
Elton Lazzarin <elton.lazzarin@outlook.com>
2020-12-22 08:34:08 -0300

    customize input component

--- src/components/Input/styles.js
+++ src/components/Input/styles.js
@@ -1,23 +1,17 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-  label {
-    font-size: 14px;
-    font-weight: bold;
-    color: var(--dark-gray);
-  }
-
-  input {
-    height: 40px;
-    width: 384px;
-    padding: 0 10px;
-    margin-bottom: 32px;
-    font-size: 16px;
-    color: var(--light-gray);
-    border: 1px solid var(--dark-gray);
-  }
+const Input = styled.input`
+  height: 40px;
+  width: ${(props) => (props.small ? '280px' : '384px')};
+  padding: 0 10px;
+  margin-bottom: ${(props) => (props.small ? '0px' : '32px')};
+  font-size: 16px;
+  color: var(--light-gray);
+  border: 1px solid var(--medium-gray);
 
   &::placeholder {
     color: var(--light-gray);
   }
 `;
+
+export default Input;
