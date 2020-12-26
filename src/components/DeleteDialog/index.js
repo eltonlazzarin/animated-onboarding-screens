f1febf3c41a8c786f978e768982f14614f852da0
Elton Lazzarin <elton.lazzarin@outlook.com>
2020-12-25 23:01:39 -0300

    changed simple states to be managed by context

--- src/components/DeleteDialog/index.js
+++ src/components/DeleteDialog/index.js
@@ -1,18 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import api from '../../services/api';
+import { Context } from '../../store';
 
 import Button, { LightButton } from '../Button/styles';
 
 import Container from './styles';
 
-export default function DeleteDialog({
-  setShowModal,
-  setDeleteNaver,
-  setConfirmation,
-  id,
-}) {
-  async function getNaverData() {
+export default function DeleteDialog({ id }) {
+  const [, dispatch] = useContext(Context);
+
+  async function handleGetNaverData() {
     const token = localStorage.getItem('@Navedex:Token');
 
     await api.delete(`navers/${id}`, {
@@ -21,9 +19,13 @@ export default function DeleteDialog({
       },
     });
 
-    setDeleteNaver(false);
-    setConfirmation(true);
-    setShowModal(false);
+    dispatch({ type: 'SET_DELETE_NAVER_DIALOG', payload: false });
+    dispatch({ type: 'SET_CONFIRMATION_DIALOG', payload: true });
+    dispatch({ type: 'SET_SHOW_MODAL', payload: false });
+  }
+
+  function handleCancelDeleteDialog() {
+    dispatch({ type: 'SET_DELETE_NAVER_DIALOG', payload: false });
   }
 
   return (
@@ -33,10 +35,8 @@ export default function DeleteDialog({
         <p>Tem certeza que deseja excluir este Naver?</p>
 
         <footer>
-          <LightButton onClick={() => setDeleteNaver(false)}>
-            Cancelar
-          </LightButton>
-          <Button onClick={getNaverData}>Excluir</Button>
+          <LightButton onClick={handleCancelDeleteDialog}>Cancelar</LightButton>
+          <Button onClick={handleGetNaverData}>Excluir</Button>
         </footer>
       </div>
     </Container>
