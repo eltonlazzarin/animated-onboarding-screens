5e7de2011dbaf3cabe14356cdd2cfde390730ef8
Elton Lazzarin <elton.lazzarin@outlook.com>
2020-12-26 15:29:44 -0300

    created styles to empty naver list component

---
+++ src/components/EmptyNaverList/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: block;
+  text-align: center;
+  padding-top: 133px;
+
+  img {
+    width: 4.43rem;
+    height: 4.18rem;
+  }
+`;
