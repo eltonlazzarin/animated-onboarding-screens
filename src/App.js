86aa1e0e76542c61da602a0d007bfb0fe26be0c7
Elton Lazzarin <elton.lazzarin@outlook.com>
2020-12-25 22:59:06 -0300

    add store component to wrapper all routes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import Routes from './routes';
 import GlobalStyles from './styles/GlobalStyles';
 
+import Store from './store';
+
 function App() {
   return (
-    <>
+    <Store>
       <Routes />
       <GlobalStyles />
-    </>
+    </Store>
   );
 }
 
