b19265f1c5084c7a08fa705ff37994a8f77a3f5c
Elton Lazzarin <elton.lazzarin@outlook.com>
2020-12-25 23:01:08 -0300

    changed simple states to be managed by context

--- src/components/ConfirmationDialog/index.js
+++ src/components/ConfirmationDialog/index.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { AiOutlineClose } from 'react-icons/ai';
+
+import { Context } from '../../store';
 
 import Container from './styles';
 
-export default function ConfirmationDialog({ setConfirmation, action }) {
+export default function ConfirmationDialog({ action }) {
+  const [, dispatch] = useContext(Context);
+
+  function handleCloseConfirmationDialog() {
+    dispatch({ type: 'SET_CONFIRMATION_DIALOG', payload: false });
+  }
+
   return (
     <Container>
       <div>
         <span>
           <AiOutlineClose
-            onClick={() => setConfirmation(false)}
+            onClick={handleCloseConfirmationDialog}
             size={24}
             color="#212121"
           />
