0c78eb234471fa799908ecc3fde0901a7ff2dfe5
Elton Lazzarin <elton.lazzarin@outlook.com>
2020-12-26 15:48:50 -0300

    fixed size nave image

--- src/components/EmptyNaverList/styles.js
+++ src/components/EmptyNaverList/styles.js
@@ -1,12 +1,14 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  display: block;
-  text-align: center;
-  padding-top: 133px;
+  span {
+    display: block;
+    text-align: center;
+    padding-top: 8.31rem;
 
-  img {
-    width: 4.43rem;
-    height: 4.18rem;
+    img {
+      width: 4.43rem;
+      height: 4.18rem;
+    }
   }
 `;
