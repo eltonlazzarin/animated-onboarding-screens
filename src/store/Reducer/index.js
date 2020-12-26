4d7745658667290ed50a78970c84edc56edcbc17
Elton Lazzarin <elton.lazzarin@outlook.com>
2020-12-25 22:57:28 -0300

    created reduce to manage states

---
+++ src/store/Reducer/index.js
@@ -0,0 +1,34 @@
+const Reducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_NAVERS':
+      return {
+        ...state,
+        navers: action.payload,
+      };
+    case 'SET_SHOW_MODAL':
+      return {
+        ...state,
+        showModal: action.payload,
+      };
+    case 'SET_CONFIRMATION_DIALOG':
+      return {
+        ...state,
+        confirmation: action.payload,
+      };
+    case 'SET_DELETE_NAVER_DIALOG':
+      return {
+        ...state,
+        deleteNaver: action.payload,
+      };
+    case 'SET_ERROR_DIALOG':
+      return {
+        ...state,
+        errorMessage: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default Reducer;
